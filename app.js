d57011a53b0590275107bd5fc3321dfa94178489
whitebear05 <woojs0505@naver.com>
2021-10-04 19:42:24 +0900

    Studied: return function

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const calculator = {
-  plus: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  multiple: function (a, b) {
-    console.log(a * b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = 96;
+
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
