2471dc6c032b53cc94f0fa2ab6d9f93b93cd5969
whitebear05 <woojs0505@naver.com>
2021-10-04 20:22:51 +0900

    Studied: conditionals

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const age = parseInt(prompt("How old are you?"));
 
 if (isNaN(age)) {
   console.log("Please write a number.");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 100) {
+  console.log("You can drink!");
 } else {
-  console.log("Thank you for writing your age.");
+  console.log("Write the currect number.");
 }
