2cab72d8d9a642a911da267244d4dbbc9433c9cb
whitebear05 <woojs0505@naver.com>
2021-10-04 21:11:12 +0900

    Studied document object and getElementById

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
-if (isNaN(age)) {
-  console.log("Please write a number.");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 100) {
-  console.log("You can drink!");
-} else {
-  console.log("Write the currect number.");
-}
+title.innerText = "Hoho!!";
