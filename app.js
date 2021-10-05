1c0df68aded4cd79124edb98c3f369b75e60b001
whitebear05 <woojs0505@naver.com>
2021-10-05 14:14:39 +0900

    Studied: divide css and js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
