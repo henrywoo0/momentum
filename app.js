c0062d37a3caadb0cf27c87eafe3fe9e2e26f8e9
whitebear05 <woojs0505@naver.com>
2021-10-05 10:21:57 +0900

    Studied window object

--- app.js
+++ app.js
@@ -9,9 +9,30 @@ function handleMouseLeave() {
 }
 
 function handleOnClick() {
-  h1.style.color = "blue";
+  h1.style.color = "white";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("WIFI Connected!!");
 }
 
 h1.addEventListener("mouseenter", handleMouseEnter);
 h1.addEventListener("mouseleave", handleMouseLeave);
 h1.onclick = handleOnClick;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
