3bf50f96c9008640e8e2aa9a435a155b9babe95f
whitebear05 <woojs0505@naver.com>
2021-10-05 11:30:36 +0900

    Studied change css event listener

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const h1 = document.querySelector(".hello h1");
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!!";
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone😂😂";
-}
-
-function handleOnClick() {
-  h1.style.color = "white";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("WIFI Connected!!");
-}
-
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-h1.onclick = handleOnClick;
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
