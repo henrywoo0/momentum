38d466a0dede961e812e47f3bc3eda2a4f744bc6
whitebear05 <woojs0505@naver.com>
2021-10-07 13:36:05 +0900

    Studied: preventDefault again

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
