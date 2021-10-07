bfd3f9be07a959f10b2261a44e065d0e2463ff0a
whitebear05 <woojs0505@naver.com>
2021-10-07 18:20:45 +0900

    Setting todo

--- js/background.js
+++ js/background.js
@@ -5,5 +5,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.alt = "background image";
 
 document.body.appendChild(bgImage);
