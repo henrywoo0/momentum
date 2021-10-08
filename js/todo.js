247c944b8c7e962b41d3a5c9406eedbcf32a1723
whitebear05 <woojs0505@naver.com>
2021-10-08 10:41:10 +0900

    Update todos

--- js/todo.js
+++ js/todo.js
@@ -13,12 +13,15 @@ function saveToDos() {
 function deleteToDo(event) {
   const li = event.target.parentNode;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newToDo) {
   const li = document.createElement("li");
+  li.id = newToDo.id;
   const span = document.createElement("span");
-  span.innerText = newToDo;
+  span.innerText = newToDo.text;
   const button = document.createElement("button");
   button.addEventListener("click", deleteToDo);
   button.innerText = "❌";
@@ -31,8 +34,12 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newToDo);
-  paintToDo(newToDo);
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
   saveToDos();
 }
 
