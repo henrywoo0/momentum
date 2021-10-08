7c417594d7c93362da77d653d16dbc33050fef18
whitebear05 <woojs0505@naver.com>
2021-10-08 11:43:13 +0900

    Update: temp

--- js/weather.js
+++ js/weather.js
@@ -9,7 +9,7 @@ function onGeoOk(position) {
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
       city.innerText = data.name;
     });
 }
